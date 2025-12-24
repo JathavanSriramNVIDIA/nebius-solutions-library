ca60c6d680453ee6a13e36fbdcf4afd33706cc89
Barys Serankou <chessprofessor@nebius.com>
2025-12-24 15:22:22 +0100

    Use new activechecks helm values format

--- soperator/modules/slurm/locals_active_checks.tf
+++ soperator/modules/slurm/locals_active_checks.tf
@@ -2,50 +2,78 @@ locals {
   active_checks_scopes = {
     # Scope for dev clusters
     dev = {
-      dcgmiDiagR2 = {
+      dcgmi-diag-r2 = {
         runAfterCreation = false
       }
-      dcgmiDiagR3 = {
+      dcgmi-diag-r3 = {
         runAfterCreation = false
       }
-      sshCheck = {
-        numOfLoginNodes = var.node_count.login
+      ssh-check = {
+        k8sJobSpec = {
+          jobContainer = {
+            env = [{
+              name: "NUM_OF_LOGIN_NODES",
+              value: var.node_count.login
+            }]
+          }
+        }
       }
     }
 
     # Run what is relevant in E2E
     testing = {
-      dcgmiDiagR3 = {
+      dcgmi-diag-r3 = {
         runAfterCreation = false
       }
-      sshCheck = {
-        numOfLoginNodes = var.node_count.login
+      ssh-check = {
+        k8sJobSpec = {
+          jobContainer = {
+            env = [{
+              name: "NUM_OF_LOGIN_NODES",
+              value: var.node_count.login
+            }]
+          }
+        }
       }
     }
 
     # Check the provisioned cluster, but don't run health-checks that take long
     prod_quick = {
-      allReducePerfNCCLInDocker = {
+      all-reduce-perf-nccl-in-docker = {
         runAfterCreation = false
       }
-      dcgmiDiagR2 = {
+      dcgmi-diag-r2 = {
         runAfterCreation = false
       }
-      dcgmiDiagR3 = {
+      dcgmi-diag-r3 = {
         runAfterCreation = false
       }
-      sshCheck = {
-        numOfLoginNodes = var.node_count.login
+      ssh-check = {
+        k8sJobSpec = {
+          jobContainer = {
+            env = [{
+              name: "NUM_OF_LOGIN_NODES",
+              value: var.node_count.login
+            }]
+          }
+        }
       }
     }
 
     # Run all available health-checks
     prod_acceptance = {
-      allReducePerfNCCLInDocker = {
+      all-reduce-perf-nccl-in-docker = {
         runAfterCreation = false
       }
-      sshCheck = {
-        numOfLoginNodes = var.node_count.login
+      ssh-check = {
+        k8sJobSpec = {
+          jobContainer = {
+            env = [{
+              name: "NUM_OF_LOGIN_NODES",
+              value: var.node_count.login
+            }]
+          }
+        }
       }
     }
   }
